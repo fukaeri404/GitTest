06b6d82bc14a38be74c8ee82fe5f2b4e1dc8726f
fukaeri404 <aungnyeinhan00@gmail.com>
2023-05-03 18:17:22 +0630

    added shoppingcart

--- src/com/hostmdy/test/HelloWorld.java
+++ src/com/hostmdy/test/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello Git");
 		System.out.println("Test Vi Editor");
 		System.out.println("Inside Dev 001 Branch");
+		System.out.println("To Test Shopping Cart");
 	}
 }
