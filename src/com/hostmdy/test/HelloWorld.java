07490a9b0a69638f3371e3f1236be4c1dd4fdcd7
fukaeri404 <111515947+fukaeri404@users.noreply.github.com>
2023-05-04 16:19:35 +0630

    Update HelloWorld.java

--- src/com/hostmdy/test/HelloWorld.java
+++ src/com/hostmdy/test/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		System.out.println("Test Vi Editor");
 		System.out.println("Inside Dev 001 Branch");
 		System.out.println("To Test Shopping Cart");
+		System.out.println("I changes from remote repository");
 	}
 }
